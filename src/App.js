90da08c29dd606184e680786d2653b77d583b972
Kamran Ahmad <kamranahmadkhan110@gmail.com>
2024-09-14 20:39:36 +0500

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { Main } from "./components/Main";
-import { Nav } from "./components/Nav";
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Routing from "./components/Routing";
+import Footer from "./components/Footer";
 function App() {
   return (
-        <>
-          <Nav/>
-          <Header/>
-          <Main/>
-          <Footer/>
-        </>
+    <>
+      <Navbar />
+      <Routing />
+      <Footer />
+    </>
   );
 }
 
